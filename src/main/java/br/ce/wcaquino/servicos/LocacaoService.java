425573a38e47fff660a1f1672bf63f27848e5cd9
RoAlencar <roalencarx@gmail.com>
2021-11-30 22:38:02 -0300

    TesteUnitario feat: Organização dos arquivos de teste

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -13,6 +13,11 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class LocacaoService {
+
+	public String vPublic;
+	protected  String vProtegido;
+	private String vPrivado;
+	String vDefault;
 	
 	public Locacao alugarFilme(Usuario usuario, Filme filme) {
 		Locacao locacao = new Locacao();
@@ -32,20 +37,5 @@ public class LocacaoService {
 		return locacao;
 	}
 
-	@Test
-	public void teste() {
-		//Cenario
-		LocacaoService service = new LocacaoService();
-		Usuario usuario = new Usuario("Usuario 1");
-		Filme filme = new Filme("Filme",2,5.0);
 
-		//Ação
-		Locacao locacao = service.alugarFilme(usuario,filme);
-
-		//Verificação
-		Assert.assertTrue(locacao.getValor() == 5.0);
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)));
-		
-	}
 }
